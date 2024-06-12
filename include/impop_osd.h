0eb172a122212f7c13bafc74832a3e522e338bd9
Conrad Parker <conrad@metadecks.org>
2024-06-13 08:09:44 +1000

    osd: add ffwd/rev speed indicators

--- include/impop_osd.h
+++ include/impop_osd.h
@@ -40,14 +40,7 @@ static inline void OSDClear() {
     osd_type = OSDType::None;
 }
 
-static inline void OSDTextV(const char* fmt, va_list args) {
-    osd_type = OSDType::Text;
-    osd_start = ImGui::GetTime();
-
-    vsnprintf(osd_text_buffer, sizeof(osd_text_buffer), fmt, args);
-
-    ImGuiIO& io = ImGui::GetIO();
-    float max_text_width = io.DisplaySize.x * 0.9f; // Let's use 90% of the screen width for text
+static inline ImVec2 OSDCalcTextSize(float max_text_width) {
     ImFont* font = ImGui::GetFont(); // Get the default font
 
     // Start with a large font size
@@ -59,6 +52,20 @@ static inline void OSDTextV(const char* fmt, va_list args) {
         osd_font_size -= 1.0f;
         text_size = font->CalcTextSizeA(osd_font_size, FLT_MAX, 0.0f, osd_text_buffer);
     }
+
+    return text_size;
+}
+
+static inline void OSDTextV(const char* fmt, va_list args) {
+    osd_type = OSDType::Text;
+    osd_start = ImGui::GetTime();
+
+    vsnprintf(osd_text_buffer, sizeof(osd_text_buffer), fmt, args);
+
+    ImGuiIO& io = ImGui::GetIO();
+    float max_text_width = io.DisplaySize.x * 0.9f; // Let's use 90% of the screen width for text
+
+    ImVec2 text_size = OSDCalcTextSize(max_text_width);
 
     // Calculate the position to center the text
     osd_text_pos = ImVec2((io.DisplaySize.x - text_size.x) * 0.5f, (io.DisplaySize.y - text_size.y) * 0.5f);
@@ -86,14 +93,39 @@ static inline void OSDStop() {
     osd_start = ImGui::GetTime();
 }
 
-static inline void OSDForward() {
-    osd_type = OSDType::Forward;
-    osd_start = ImGui::GetTime();
+static inline void OSDSetSpeed(int speed) {
+    osd_forward_reverse_speed = speed;
+
+    if (speed > 1) {
+        snprintf(osd_text_buffer, sizeof(osd_text_buffer), "x%d", speed);
+        ImGuiIO& io = ImGui::GetIO();
+        const ImVec2 sz = io.DisplaySize;
+        float max_text_width = sz.x * 0.125f;
+
+        ImVec2 text_size = OSDCalcTextSize(max_text_width);
+
+        ImFont* font = ImGui::GetFont(); // Get the default font
+        float ascent = font->Ascent * osd_font_size / font->FontSize;
+
+        float offset = std::min(sz.x, sz.y)*0.25f;
+        osd_text_pos = ImVec2(sz.x*0.5f + offset, sz.y*0.5f + offset - ascent);
+    } else {
+        osd_text_buffer[0] = '\0';
+    }
 }
 
-static inline void OSDReverse() {
+static inline void OSDForward(int speed=1) {
+    osd_type = OSDType::Forward;
+    osd_start = ImGui::GetTime();
+
+    OSDSetSpeed(speed);
+}
+
+static inline void OSDReverse(int speed=1) {
     osd_type = OSDType::Reverse;
     osd_start = ImGui::GetTime();
+
+    OSDSetSpeed(speed);
 }
 
 static inline void OSDShow() {
@@ -115,11 +147,11 @@ static inline void OSDShow() {
     ImGuiIO& io = ImGui::GetIO();
     const ImVec2 sz = io.DisplaySize;
 
-    const ImVec2 center = ImVec2(sz.x/2.0, sz.y/2.0);
+    const ImVec2 center = ImVec2(sz.x*0.5f, sz.y*0.5f);
     const float side = std::min(center.x, center.y);
     const ImVec2 square = ImVec2(side, side);
-    const ImVec2 square_min = ImVec2(center.x - side/2.0, center.y - side/2.0);
-    const ImVec2 square_max = ImVec2(center.x + side/2.0, center.y + side/2.0);
+    const ImVec2 square_min = ImVec2(center.x - side*0.5f, center.y - side*0.5f);
+    const ImVec2 square_max = ImVec2(center.x + side*0.5f, center.y + side*0.5f);
 
     // Access the foreground draw list (drawn after all windows)
     ImDrawList* draw_list = ImGui::GetForegroundDrawList();
@@ -163,6 +195,9 @@ static inline void OSDShow() {
                 ImVec2 p3 = ImVec2(center.x, square_max.y);
                 draw_list->AddTriangleFilled(p1, p2, p3, color);
             }
+            if (osd_forward_reverse_speed > 1) {
+                draw_list->AddText(nullptr, osd_font_size, osd_text_pos, color, osd_text_buffer);
+            }
             break;
         case OSDType::Reverse:
             {
@@ -174,6 +209,9 @@ static inline void OSDShow() {
             {
                 ImVec2 p1 = ImVec2(square_max.x, square_min.y);
                 draw_list->AddTriangleFilled(p1, center, square_max, color);
+            }
+            if (osd_forward_reverse_speed > 1) {
+                draw_list->AddText(nullptr, osd_font_size, osd_text_pos, color, osd_text_buffer);
             }
             break;
         default:
